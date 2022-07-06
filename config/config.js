0748c8a8f7de745bdc5a45cb752fc98093af52d3
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-07-06 17:52:19 +0300

    added client user components

--- config/config.js
+++ config/config.js
@@ -5,9 +5,9 @@ const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/duka', 
-  stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
-  stripe_test_secret_key: process.env.STRIPE_TEST_SECRET_KEY,
-  stripe_test_api_key: process.env.STRIPE_TEST_API_KEY
+  stripe_connect_test_client_id: process.env.STRIPE_CONNECT_TEST_CLIENT_ID || "YOUR_stripe_connect_test_client",
+  stripe_test_secret_key: process.env.STRIPE_TEST_SECRET_KEY || "YOUR_stripe_test_secret_key",
+  stripe_test_api_key: process.env.STRIPE_TEST_API_KEY || "YOUR_stripe_test_api_key",
 }
 
 export default config;
