c7dcdb3f7f45a199dad5252c5b924d8f865d84b7
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-07-07 15:03:44 +0300

    test uri

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
+    'mongodb+srv://kasiliwachiye:RabbitHole4747@cluster0.xewhkin.mongodb.net/?retryWrites=true&w=majority' ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
     '/duka',
