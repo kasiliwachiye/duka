1251b4e2e1cce2072bf4505df130e68504131549
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-07-07 16:20:35 +0300

    test production

--- config/config.js
+++ config/config.js
@@ -7,9 +7,9 @@ const config = {
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
     '/duka',
-  stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
-  stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
-  stripe_test_api_key: 'YOUR_stripe_test_api_key' 
+  stripe_connect_test_client_id: 'ca_FkyHCg7X8mlvCUdMDao4mMxagUfhIwXb',
+  stripe_test_secret_key: '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',
+  stripe_test_api_key: 'pk_live_51IdvJgK5HXK2AlvIpE92GtBv8lqz6UDrnneZaOSSFsy4Kf7vI2DWodkpAevs3S8riKBgYYXAhMtgJhVDjjkRAkVP00H6c6gwd1'
 }
 
 export default config
