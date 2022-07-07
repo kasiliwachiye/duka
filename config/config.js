c49ddc9ca5b7c5d023469df5fe49f570929309fc
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-07-07 14:33:37 +0300

    production test

--- config/config.js
+++ config/config.js
@@ -1,15 +1,11 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-  process.env.MONGO_HOST ||
-  'mongodb://' + (process.env.IP || 'localhost') + ':' +
-  (process.env.MONGO_PORT || '27017') +
-  '/duka', 
-  stripe_connect_test_client_id: process.env.STRIPE_CONNECT_TEST_CLIENT_ID || 'YOUR_stripe_connect_test_client',
-  stripe_test_secret_key: process.env.STRIPE_TEST_SECRET_KEY ||'YOUR_stripe_test_secret_key',
-  stripe_test_api_key: process.env.STRIPE_TEST_API_KEY ||'YOUR_stripe_test_api_key',
+  mongoUri: 'mongodb+srv://kasiliwachiye:RabbitHole4747@cluster0.xewhkin.mongodb.net/?retryWrites=true&w=majority', 
+  stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
+  stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
+  stripe_test_api_key: 'YOUR_stripe_test_api_key',
 }
 
 export default config;
