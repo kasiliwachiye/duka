7f6cf1beab14345cc891bc75362f9ae11fdcf983
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-07-07 12:15:00 +0300

    set up for production mode

--- client/theme.js
+++ client/theme.js
@@ -1,24 +1,24 @@
 import { createMuiTheme } from '@material-ui/core/styles'
-import { blueGrey, lightGreen } from '@material-ui/core/colors'
+import { cyan, blue } from '@material-ui/core/colors'
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-    light: '#8eacbb',
-    main: '#607d8b',
-    dark: '#34515e',
+    light: '#DBE2EF',
+    main: '#3F72AF',
+    dark: '#364F6B',
     contrastText: '#fff',
   },
   secondary: {
-    light: '#e7ff8c',
-    main: '#b2ff59',
-    dark: '#7ecb20',
+    light: '#EFBBCF',
+    main: '#C3AED6',
+    dark: '#8675A9',
     contrastText: '#000',
   },
-    openTitle: blueGrey['400'],
-    protectedTitle: lightGreen['400'],
+    openTitle: blue['700'],
+    protectedTitle: cyan['700'],
     type: 'light'
   }
 })
 
-  export default theme
+export default theme  
